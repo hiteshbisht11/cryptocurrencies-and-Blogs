f8643756feabc865e18de0be60655ab0860d5891
Hitesh <hiteshbisht565@gmail.com>
2023-06-11 12:19:01 +0530

    first commit

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import styles from './Footer.module.css'
+function Footer(){
+    return(
+        <div>
+            <p className={styles.footer}> Coin Bounce 2023</p>
+        </div>
+    );
+}
+export default Footer;
---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,98 @@
+import { NavLink } from "react-router-dom";
+import styles from "./Navbar.module.css";
+import { useSelector } from "react-redux";
+import { signout } from "../../api/internal";
+import { resetUser } from "../../store/userSlice";
+import { useDispatch } from "react-redux";
+
+function Navbar() {
+  const dispatch = useDispatch();
+
+  const isAuthenticated = useSelector((state) => state.user.auth);
+
+  const handleSignout = async () => {
+    await signout();
+    dispatch(resetUser());
+  };
+
+  return (
+    <>
+    <div className={styles.navi}>
+      <nav className={styles.navbar}>
+        <NavLink to="/" className={`${styles.logo} ${styles.inActiveStyle}`}>
+          <p className={styles.text}>Coin Bounce</p>
+        </NavLink>
+
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive ? styles.activeStyle : styles.inActiveStyle
+          }
+        >
+          Home
+        </NavLink>
+
+        <NavLink
+          to="crypto"
+          className={({ isActive }) =>
+            isActive ? styles.activeStyle : styles.inActiveStyle
+          }
+        >
+          Cryptocurrencies
+        </NavLink>
+
+        <NavLink
+          to="blogs"
+          className={({ isActive }) =>
+            isActive ? styles.activeStyle : styles.inActiveStyle
+          }
+        >
+          Blogs
+        </NavLink>
+
+        <NavLink
+          to="submit"
+          className={({ isActive }) =>
+            isActive ? styles.activeStyle : styles.inActiveStyle
+          }
+        >
+          Submit a blog
+        </NavLink>
+
+        {isAuthenticated ? (
+          <div>
+            <NavLink>
+              <button className={styles.signOutButton} onClick={handleSignout}>
+                Sign Out
+              </button>
+            </NavLink>
+          </div>
+        ) : (
+          <div>
+            <NavLink
+              to="login"
+              className={({ isActive }) =>
+                isActive ? styles.activeStyle : styles.inActiveStyle
+              }
+            >
+              <button className={styles.logInButton}>Log In</button>
+            </NavLink>
+
+            <NavLink
+              to="signup"
+              className={({ isActive }) =>
+                isActive ? styles.activeStyle : styles.inActiveStyle
+              }
+            >
+              <button className={styles.signUpButton}>Sign Up</button>
+            </NavLink>
+          </div>
+        )}
+      </nav>
+      <div className={styles.separator}></div>
+    </div>
+    </>
+  );
+}
+
+export default Navbar;
